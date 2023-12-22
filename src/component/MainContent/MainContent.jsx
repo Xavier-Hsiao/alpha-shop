ec7cd865593e5df09a4a6e8cbbdc9ea2490f18c7
Xavier-Hsiao <tim88776@gmail.com>
2023-12-22 16:01:59 +0800

    Build: add Cart basic markup and styling

--- src/component/MainContent/MainContent.jsx
+++ src/component/MainContent/MainContent.jsx
@@ -3,6 +3,7 @@ import Step1 from "./Step1";
 import Step2 from "./Step2";
 import Step3 from "./Step3";
 import ProgressControl from "./ProgressControl";
+import Cart from "./cart.jsx";
 import styles from "../../style/MainContent.module.scss";
 
 export default function MainContent() {
@@ -17,7 +18,9 @@ export default function MainContent() {
             <Step3 />
             <ProgressControl />
           </div>
-          <div className={styles.rightMainContent}></div>
+          <div className={styles.rightMainContent}>
+            <Cart />
+          </div>
         </div>
       </section>
     </>
