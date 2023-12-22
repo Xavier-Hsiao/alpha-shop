02370799c23dfff2c4fba04fcf0ce6943abbeb27
Xavier-Hsiao <tim88776@gmail.com>
2023-12-22 16:34:18 +0800

    Feat: complete cart list rendering

--- src/component/MainContent/Cart/cart.jsx
+++ src/component/MainContent/Cart/cart.jsx
@@ -1,17 +1,36 @@
-import Product from "./product";
+import Product from "./Product";
 import styles from "../../../style/cart.module.scss";
 
+const data = [
+  {
+    id: "1",
+    name: "貓咪罐罐",
+    img: "https://picsum.photos/300/300?text=1",
+    price: 100,
+    quantity: 2,
+  },
+  {
+    id: "2",
+    name: "貓咪干干",
+    img: "https://picsum.photos/300/300?text=2",
+    price: 200,
+    quantity: 1,
+  },
+];
+
 export default function Cart() {
+  // Generate product dynamically
+  const productList = data.map((product) => (
+    <Product key={product.id} product={product} />
+  ));
+  // Return Cart component
   return (
     <section className={styles.cartContainer}>
       <h3 className={styles.cartTitle}>購物籃</h3>
       {/* Product section */}
       <section className={styles.productContainer}>
         {/* Product card area*/}
-        <ul className={styles.productList}>
-          <Product />
-          <Product />
-        </ul>
+        <ul className={styles.productList}>{productList}</ul>
       </section>
       {/* Cart info section */}
       <section className={styles.cartInfoArea}>
--- src/component/MainContent/Cart/product.jsx
+++ src/component/MainContent/Cart/product.jsx
@@ -1,23 +1,23 @@
 import styles from "../../../style/Product.module.scss";
 
-export default function Product() {
+export default function Product({ product }) {
   return (
     <li className={styles.product}>
       <div className={styles.imageContainer}>
         <img
-          src="https://picsum.photos/300/300?text=1"
+          src={product.img}
           alt="product-image"
           className={styles.productImage}
         />
       </div>
       <div className={styles.productInfo}>
-        <div className={styles.productName}>貓貓罐罐</div>
-        <div className={styles.productPrice}>$100</div>
+        <div className={styles.productName}>{product.name}</div>
+        <div className={styles.productPrice}>{product.price}</div>
         <div className={styles.actionBtnGroup}>
           <a href="#" className={`${styles.actionBtn} ${styles.minus}`}>
             &#8722;
           </a>
-          <p className={styles.productQty}>2</p>
+          <p className={styles.productQty}>{product.quantity}</p>
           <a href="#" className={`${styles.actionBtn} ${styles.plus}`}>
             &#43;
           </a>
