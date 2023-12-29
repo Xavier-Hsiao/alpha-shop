93133c905799f6f04307707002cae99be0690a20
Xavier-Hsiao <tim88776@gmail.com>
2023-12-29 15:18:13 +0800

    Feat: add step from control button feature on ProgressControl component

--- src/component/MainContent/Form/ProgressControl.jsx
+++ src/component/MainContent/Form/ProgressControl.jsx
@@ -1,10 +1,14 @@
 import styles from "../../../style/ProgressControl.module.scss";
 
-export default function ProgressControl() {
+export default function ProgressControl({ onNext, onPrevious }) {
   return (
     <div className={styles.progressControl}>
-      <button className={styles.previousBtn}>上一步</button>
-      <button className={styles.nextBtn}>下一步</button>
+      <button className={styles.previousBtn} onClick={onPrevious}>
+        上一步
+      </button>
+      <button className={styles.nextBtn} onClick={onNext}>
+        下一步
+      </button>
     </div>
   );
 }
--- src/component/MainContent/MainContent.jsx
+++ src/component/MainContent/MainContent.jsx
@@ -2,21 +2,39 @@ import StepProgress from "./Form/StepProgress.jsx";
 import Step1 from "./Form/Step1";
 import Step2 from "./Form/Step2";
 import Step3 from "./Form/Step3";
+import { useState } from "react";
 import ProgressControl from "./Form/ProgressControl.jsx";
 import Cart from "./Cart/cart.jsx";
 import styles from "../../style/MainContent.module.scss";
 
 export default function MainContent() {
+  // Define state
+  const [step, setStep] = useState(1);
+
+  // Define event handlers
+  function handleNext() {
+    if (step < 3) {
+      setStep((step) => step + 1);
+    }
+  }
+
+  function handlePrevious() {
+    if (step > 1) {
+      setStep((step) => step - 1);
+    }
+  }
+
   return (
     <>
       <section className={styles.mainContent}>
         <div className={styles.contentWrapper}>
           <div className={styles.leftMainContent}>
             <StepProgress />
-            {/* <Step1 /> */}
-            {/* <Step2 /> */}
-            <Step3 />
-            <ProgressControl />
+            {/* Conditional rendering of step forms */}
+            {step === 1 && <Step1 />}
+            {step === 2 && <Step2 />}
+            {step === 3 && <Step3 />}
+            <ProgressControl onNext={handleNext} onPrevious={handlePrevious} />
           </div>
           <div className={styles.rightMainContent}>
             <Cart />
